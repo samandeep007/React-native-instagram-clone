68feec09d604d5f0602510d95c280902dea73795
samandeep007 <115439036+samandeep007@users.noreply.github.com>
2024-08-07 10:53:52 -0600

    chore: Update @react-navigation/native-stack to version 6.11.0

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,33 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
-import {NavigationContainer} from '@react-navigation/native'
-import {createStackNavigator} from '@react-navigation/stack'
+
+// Navigation Modules 
+import {NavigationContainer} from '@react-navigation/native' 
+import {createNativeStackNavigator} from '@react-navigation/native-stack'
+
+// Screens
+import Home from './screens/Home';
+import Details from './screens/Details'
+
+export type RootStackParamList = {
+  Home: undefined; //will expect nothing
+  Details: {productId: string}
+}
+
+const Stack  = createNativeStackNavigator<RootStackParamList>(); //Stack Navigator
+
+
+
 
 export default function App() {
   return (
-    <SafeAreaView>
-    <View>
-      <Text>App</Text>
-    </View>
-    </SafeAreaView>
+     <NavigationContainer>
+      <Stack.Navigator initialRouteName='Home'>
+        <Stack.Screen name='Home' component={Home} options={{title: "Trending Products"}}/>
+        <Stack.Screen name='Details' component={Details} options={{title: "Product Details"}}/>
+      </Stack.Navigator>
+      </NavigationContainer>
   )
 }
 
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,23 +1,101 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Alert, Pressable, ScrollView, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
 export default function Home() {
-  return (
-    <View>
-      <Text>Home</Text>
-    </View>
-  )
+    return (
+        <ScrollView contentContainerStyle={{ flex: 1 }}>
+            <View style={styles.container}>
+                <View style={{flexDirection: "row", gap: 1}}>
+                    
+                         <Pressable style={styles.product} onPress={() => Alert.alert("Hello", "Your story is not available right now")}>
+                        <Text style={styles.productTitle}>M</Text>
+                    </Pressable>
+                    
+                    <ScrollView horizontal={true}>
+                        <View style={styles.productSection}>
+                            <Pressable style={styles.product} onPress={() => Alert.alert("Hello", "The story is not available right now")}>
+                                <Text style={styles.productTitle}>A</Text>
+                            </Pressable>
+                            <Pressable style={styles.product} onPress={() => Alert.alert("Hello", "The story is not available right now")}>
+                                <Text style={styles.productTitle}>B</Text>
+                            </Pressable>
+                            <Pressable style={styles.product} onPress={() => Alert.alert("Hello", "The story is not available right now")}>
+                                <Text style={styles.productTitle}>C</Text>
+                            </Pressable>
+                            <Pressable style={styles.product} onPress={() => Alert.alert("Hello", "The story is not available right now")}>
+                                <Text style={styles.productTitle}>D</Text>
+                            </Pressable>
+                            <Pressable style={styles.product} onPress={() => Alert.alert("Hello", "The story is not available right now")}>
+                                <Text style={styles.productTitle}>E</Text>
+                            </Pressable>
+                        </View>
+
+                    </ScrollView>
+                </View>
+                <View style={styles.banner}>
+                    <Text style={styles.bannerTitle}>Amazing Discounts. Avail Today!!</Text>
+                    <Text style={styles.bannerDescription}>Hurry Up!! Offers are valid for limited time only</Text>
+                </View>
+            </View>
+
+        </ScrollView>
+    )
 }
 
 const styles = StyleSheet.create({
-     container: {
+    container: {
         flex: 1,
         backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-     },
+        padding: 16,
+        gap: 16
+    },
 
-     smallText: {
+
+    productSection: {
+        flexDirection: "row",
+        gap: 10,
+        maxHeight: 200,
+        paddingHorizontal: 10,
+      
+    },
+
+    product: {
+        width: 90,
+        height: 90,
+        backgroundColor: "white",
+        borderColor: "purple",
+        borderWidth: 4,
+        borderRadius: 90 / 2,
+        justifyContent: "center",
+        alignItems: "center"
+    },
+
+    productTitle: {
         color: "#000000",
-     }
+        fontSize: 28
+    },
+
+    banner: {
+        flex: 1,
+        maxHeight: 140,
+        width: "100%",
+        borderColor: "gray",
+        borderWidth: 2,
+        borderRadius: 16,
+        padding: 16,
+        gap: 10
+    },
+
+    bannerTitle: {
+        fontSize: 23,
+        color: "#000000",
+        fontWeight: "600",
+
+    },
+
+    bannerDescription: {
+        fontSize: 14,
+        color: "#000000"
+    }
+
 })
